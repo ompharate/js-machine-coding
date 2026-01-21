2b71f2d9a4a39e21950b6b81893df690ce084e6d
om pharate <72200400+ompharate@users.noreply.github.com>
2026-01-21 06:20:25 +0000

    implement once function to ensure single execution of a callback

---
+++ JS/1.js
@@ -0,0 +1,24 @@
+// Write a function that runs only once, no matter how many times it is called.
+
+function once(fn) {
+    let called = false;
+    let result;
+    return function (...args) {
+        if (!called) {
+        console.log("recalculating")
+        result = fn.apply(this,args);    
+        called = true;
+    }
+    return result;
+}
+}
+
+
+function add(a,b) {
+    return a+b;
+}
+
+let heavyCalculation = once(add);
+
+console.log(heavyCalculation(5,8))
+console.log(heavyCalculation(5,8));
